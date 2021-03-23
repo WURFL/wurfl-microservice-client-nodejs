ecf6c83828bba2e2413832c68ead57aa9a33290b
andrea <castello.a@gmail.com>
2021-03-23 14:13:41 +0100

    WPC-232: updated massive detection and web server examples

--- webserver_example.js
+++ webserver_example.js
@@ -17,50 +17,51 @@
 // PLEASE, notice that this is a server, so it will keep on running until you kill it or you make a GET request using the parameter shut=y
 
 // this must be used when executing directly from source code downloaded from git repo
-//var client = require('./src/app/wmclient');
+let wmclient = require('./src/app/wmclient');
 
 // this must be used when executing the example by installing wmclient using  'npm install wmclient'
-var client = require('wmclient');
+//var wmclient = require('wmclient');
 
-var http = require('http');
-var url = require('url');
+let http = require('http');
+let url = require('url');
 
 console.log('Running with node version ' + process.version)
 
-http.createServer(function (req, res) {
+http.createServer(async (req, res) => {
 
     // First we need to create a WM client instance, to connect to our WM server API at the specified host and port.
-    client.create('http:', 'localhost', '8080', '', function (result, error) {
+    let client
+    try {
+        client = await wmclient.create('http:', 'localhost', '8080', '')
 
-        if(error!== undefined){
-            console.log('Client creation failed, shutting down')
-            process.exit();
-        }
+    } catch (error) {
+        console.log(`Client creation failed, shutting down: ${error.message}`)
+        process.exit()
+    }
 
-        // Perform a device detection calling WM server API using the current request
-        result.lookupRequest(req, function (device, err) {
+    // Perform a device detection calling WM server API using the current request
+    let device
+    try {
+        device = await client.lookupRequest(req)
+    } catch (error) {
+        console.log(`Response error: ${error.message}`)
+        res.writeHead(500, {'Content-Type': 'application/json'})
+        res.write(`LOOKUP Error: ' ${error.message}`)
+    }
 
-            if(err!==undefined){
-                console.log('Response error: ' + err.message);
-                res.writeHead(500, {'Content-Type': 'application/json'});
-                res.write('LOOKUP Error: ' + err.message);
-                return;
-            }
+    let url_parts = url.parse(req.url, true)
+    let query = url_parts.query
+    let shut = query['shut']
+    // Let's get the device capabilities and print some of them
+    console.log('Detected: ' + device.capabilities['brand_name'] + ' ' + device.capabilities['model_name'])
+    // THIS sends the response to whichever tool you use to call this server (browser,postman or another piece of software
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write('LOOKUP REQUEST RESULT: ' + device.capabilities['brand_name'] + ' ' + device.capabilities['model_name'])
+    res.end();
 
-            var url_parts = url.parse(req.url, true);
-            var query = url_parts.query;
-            var shut = query['shut'];
-            // Let's get the device capabilities and print some of them
-            console.log('Detected: ' + device.capabilities['brand_name'] + ' ' + device.capabilities['model_name'])
-            // THIS sends the response to whichever tool you use to call this server (browser,postman or another piece of software
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            res.write('LOOKUP REQUEST RESULT: ' + device.capabilities['brand_name'] + ' ' + device.capabilities['model_name']);
-            res.end();
-
-            if(shut!== undefined && shut === 'y') {
-                console.log('Shutting down server')
-                process.exit();
-            }
-        });
-    });
-}).listen(19080);
+    if (shut !== undefined && shut === 'y') {
+        console.log('Sent termination parameter: shutting down server')
+        process.exit();
+    }
+}).
+listen(19080)
